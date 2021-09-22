6974ccff35441e3ed6db243b07249556b8aef640
MauroT <mauro.imamura@outlook.com>
2021-09-22 17:19:03 -0300

    formulario cadastro de imovel

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,4 +2,13 @@ class PropertiesController < ApplicationController
     def show
         @property = Property.find(params[:id])
     end
+
+    def new
+        @property = Property.new
+    end
+
+    def create
+        prop = Property.create(params.require(:property).permit(:title, :description, :rooms, :bathrooms, :pets, :parking_slot, :daily_rate))
+        redirect_to prop
+    end
 end
---
+++ spec/system/visitor_register_property_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Visitor register property' do
+    it 'successfully' do
+    
+        visit root_path
+        click_on 'Cadastrar imóvel'
+        fill_in 'Título', with: 'Casa em Florianópolis'
+        fill_in 'Descrição', with: 'Ótima casa próxima a UFSC'
+        fill_in 'Quartos', with: '3'
+        fill_in 'Banheiros', with: '2'
+        fill_in 'Diária', with: 200
+        check 'Aceita Pets'
+        check 'Vaga de estacionamento'
+        click_on 'Enviar'
+
+        expect(page).to have_content('Casa em Florianópolis')
+        expect(page).to have_content('Ótima casa próxima a UFSC')
+        expect(page).to have_content('Quartos: 3')
+        expect(page).to have_content('Banheiros: 2')
+        expect(page).to have_content('Aceita Pets: Sim')
+        expect(page).to have_content('Estacionamento: Sim')
+        expect(page).to have_content('Diária: R$ 200,00')
+    end
+end
