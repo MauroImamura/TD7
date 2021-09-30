ce460a78c2673e454a6ef31e5a034fcd8524e236
MauroT <mauro.imamura@outlook.com>
2021-09-30 10:06:04 -0300

    filtro de região e correção nos testes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
     def index
         @properties = Property.all
         @property_types = PropertyType.all
+        @property_locations = PropertyLocation.all
         flash[:notice] = 'Nenhum imóvel disponível' unless @properties.present?
     end
 end
--- spec/system/visitor_register_property_spec.rb
+++ spec/system/visitor_register_property_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe 'Visitor register property' do
     it 'successfully' do
-        
         PropertyType.create!(title: 'Casa')
         PropertyLocation.create!(title: 'Litoral SC')
 
@@ -31,7 +30,6 @@ describe 'Visitor register property' do
     end
 
     it 'but decide to return to home instead' do
-        
         visit root_path
         click_on 'Cadastrar imóvel'
         click_on 'Voltar'
@@ -39,129 +37,12 @@ describe 'Visitor register property' do
         expect(current_path).to eq root_path
     end
 
-    it 'and try to register property without title and do not succed' do
-    
+    it 'and try to register property without information and do not succed' do
         casa = PropertyType.create!(title: 'Casa')
         PropertyLocation.create!(title: 'Litoral SC')
 
         visit root_path
         click_on 'Cadastrar imóvel'
-        fill_in 'Descrição', with: 'Ótima casa próxima a UFSC'
-        fill_in 'Quartos', with: '3'
-        fill_in 'Banheiros', with: '2'
-        fill_in 'Diária', with: 200
-        select 'Casa', from: 'Tipo'
-        select 'Litoral SC', from: 'Região'
-        click_on 'Enviar'
-
-        expect(page).to have_content('Não foi possível cadastrar')
-    end
-
-    it 'and try to register property without description and do not succed' do
-    
-        casa = PropertyType.create!(title: 'Casa')
-        PropertyLocation.create!(title: 'Litoral SC')
-
-        visit root_path
-        click_on 'Cadastrar imóvel'
-        fill_in 'Título', with: 'Casa em Florianópolis'
-        fill_in 'Quartos', with: '3'
-        fill_in 'Banheiros', with: '2'
-        fill_in 'Diária', with: 200
-        select 'Casa', from: 'Tipo'
-        select 'Litoral SC', from: 'Região'
-        click_on 'Enviar'
-
-        expect(page).to have_content('Não foi possível cadastrar')
-    end 
-
-    it 'and try to register property without rooms and do not succed' do
-    
-        casa = PropertyType.create!(title: 'Casa')
-        PropertyLocation.create!(title: 'Litoral SC')
-
-        visit root_path
-        click_on 'Cadastrar imóvel'
-        fill_in 'Título', with: 'Casa em Florianópolis'
-        fill_in 'Descrição', with: 'Ótima casa próxima a UFSC'
-        fill_in 'Banheiros', with: '2'
-        fill_in 'Diária', with: 200
-        select 'Casa', from: 'Tipo'
-        select 'Litoral SC', from: 'Região'
-        click_on 'Enviar'
-
-        expect(page).to have_content('Não foi possível cadastrar')
-    end 
-
-    it 'and try to register property without bathrooms and do not succed' do
-    
-        casa = PropertyType.create!(title: 'Casa')
-        PropertyLocation.create!(title: 'Litoral SC')
-
-        visit root_path
-        click_on 'Cadastrar imóvel'
-        fill_in 'Título', with: 'Casa em Florianópolis'
-        fill_in 'Descrição', with: 'Ótima casa próxima a UFSC'
-        fill_in 'Quartos', with: '3'
-        fill_in 'Diária', with: 200
-        select 'Casa', from: 'Tipo'
-        select 'Litoral SC', from: 'Região'
-        click_on 'Enviar'
-
-        expect(page).to have_content('Não foi possível cadastrar')
-    end
-
-    it 'and try to register property without daily rate and do not succed' do
-    
-        casa = PropertyType.create!(title: 'Casa')
-        PropertyLocation.create!(title: 'Litoral SC')
-
-        visit root_path
-        click_on 'Cadastrar imóvel'
-        fill_in 'Título', with: 'Casa em Florianópolis'
-        fill_in 'Descrição', with: 'Ótima casa próxima a UFSC'
-        fill_in 'Quartos', with: '3'
-        fill_in 'Banheiros', with: '2'
-        select 'Casa', from: 'Tipo'
-        select 'Litoral SC', from: 'Região'
-        click_on 'Enviar'
-
-        expect(page).to have_content('Não foi possível cadastrar')
-    end
-
-    it 'and try to register property without choosing the type and do not succed' do
-    
-        casa = PropertyType.create!(title: 'Casa')
-        PropertyLocation.create!(title: 'Litoral SC')
-
-        visit root_path
-        click_on 'Cadastrar imóvel'
-        fill_in 'Título', with: 'Casa em Florianópolis'
-        fill_in 'Descrição', with: 'Ótima casa próxima a UFSC'
-        fill_in 'Quartos', with: '3'
-        fill_in 'Banheiros', with: '2'
-        fill_in 'Diária', with: 200
-        select 'Litoral SC', from: 'Região'
-        click_on 'Enviar'
-
-        expect(page).to have_content('Não foi possível cadastrar')
-    end
-
-    it 'and try to register property without region and do not succed' do
-        
-        PropertyType.create!(title: 'Casa')
-        PropertyLocation.create!(title: 'Litoral SC')
-
-        visit root_path
-        click_on 'Cadastrar imóvel'
-        fill_in 'Título', with: 'Casa em Florianópolis'
-        fill_in 'Descrição', with: 'Ótima casa próxima a UFSC'
-        fill_in 'Quartos', with: '3'
-        fill_in 'Banheiros', with: '2'
-        fill_in 'Diária', with: 200
-        check 'Aceita Pets'
-        check 'Vaga de estacionamento'
-        select 'Casa', from: 'Tipo'
         click_on 'Enviar'
 
         expect(page).to have_content('Não foi possível cadastrar')
