c6d79db3b4264b595e9c5a07e050596f917c5b60
MauroT <mauro.imamura@outlook.com>
2021-10-01 17:24:33 -0300

    Login e Logout, testes a serem corrigidos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
         @properties = Property.all
         @property_types = PropertyType.all
         @property_locations = PropertyLocation.all
-        flash[:notice] = 'Nenhum imóvel disponível' unless @properties.present?
+        #flash[:notice] = 'Nenhum imóvel disponível' unless @properties.present?
+        #TODO mudar mensagem flash da home para nenhum imóvel cadastrado, está dando conflito com a mensagem de login
     end
 end
--- spec/system/visitor_log_in_spec.rb
+++ spec/system/visitor_log_in_spec.rb
@@ -1,20 +1,39 @@
 require 'rails_helper'
 
 describe 'Visitor log in' do
-    #TODO teste de login pronto, falta incorporar a gem
-=begin     it 'successfully' do
-        user = User.create!(email: 'ousuario@mail.com.br', password: '123456')
+    context 'as property owner' do
+        it 'successfully' do
+            property_owner = PropertyOwner.create!(email: 'ousuario@mail.com.br', password: '123456')
 
-        visit root_path
-        click_on 'Entrar'
-        fill_in 'Email', with: user.email
-        fill_in 'Senha', with: user.password
-        click_on 'Entrar'
+            visit root_path
+            click_on 'Entrar'
+            fill_in 'Email', with: property_owner.email
+            fill_in 'Senha', with: property_owner.password
+            within 'form' do
+                click_on 'Entrar'
+            end
 
-        expect(page).to have_content('Logou com sucesso')
-        expect(page).to have_content(user.email)
-        expect(page).to have_link('Logout')
-        expect(page).not_to have_content('Entrar')
-    end 
-=end
+            expect(page).to have_content('Login efetuado com sucesso!')
+            expect(page).to have_content(property_owner.email)
+            expect(page).to have_link('Logout')
+            expect(page).not_to have_content('Entrar')
+            expect(page).to have_link('Cadastrar imóvel')
+        end 
+
+        it 'and logs out' do
+            property_owner = PropertyOwner.create!(email: 'ousuario@mail.com.br', password: '123456')
+
+            login_as property_owner, scope: :property_owner
+            visit root_path
+            click_on 'Logout'
+
+            expect(page).to have_content('Saiu com sucesso')
+            expect(page).not_to have_link('Logout')
+            expect(page).to have_link('Login')
+            expect(page).not_to have_link('Cadastrar imóvel')
+            expect(page).not_to have_content(property_owner.email)
+        end
+
+        #TODO registro do property_owner: email, senha, confirmação de senha
+    end
 end
