e753356ae61b0d9bfc2854866b4c75701cf29e96
MauroT <mauro.imamura@outlook.com>
2021-10-03 23:01:02 -0300

    mudanças css

--- spec/system/visitor_new_properties_spec.rb
+++ spec/system/visitor_new_properties_spec.rb
@@ -5,8 +5,7 @@ describe 'Visitor visit homepage' do
     visit root_path
 
     expect(page).to have_css('h1', text: 'Flats')
-    expect(page).to have_css('h3', text: 'Boas vindas ao sistema de locação de '\
-                                         'propriedades')
+    expect(page).to have_content('Boas vindas ao sistema de locação de propriedades')
   end
 
 end
