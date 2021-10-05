63e5441e835e17efdd15e2659e8f3d6156e8b79c
MauroT <mauro.imamura@outlook.com>
2021-10-05 08:33:47 -0300

    interfaces para user

--- spec/system/visitor_log_in_spec.rb
+++ spec/system/visitor_log_in_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'Visitor log in' do
     context 'as property owner' do
         it 'successfully' do
-            property_owner = PropertyOwner.create!(email: 'ousuario@mail.com.br', password: '123456')
+            property_owner = PropertyOwner.create!(email: 'proprietario@mail.com.br', password: '123456')
 
             visit root_path
-            click_on 'Entrar'
+            click_on 'Entrar como proprietário'
             fill_in 'Email', with: property_owner.email
             fill_in 'Senha', with: property_owner.password
             within 'form' do
@@ -21,7 +21,7 @@ describe 'Visitor log in' do
         end 
 
         it 'and logs out' do
-            property_owner = PropertyOwner.create!(email: 'ousuario@mail.com.br', password: '123456')
+            property_owner = PropertyOwner.create!(email: 'proprietario@mail.com.br', password: '123456')
 
             login_as property_owner, scope: :property_owner
             visit root_path
@@ -29,14 +29,14 @@ describe 'Visitor log in' do
 
             expect(page).to have_content('Saiu com sucesso')
             expect(page).not_to have_link('Logout')
-            expect(page).to have_link('Entrar')
+            expect(page).to have_link('Entrar como proprietário')
             expect(page).not_to have_link('Cadastrar imóvel')
             expect(page).not_to have_content(property_owner.email)
         end
 
         it 'but decide to return to home' do
             visit root_path
-            click_on 'Entrar'
+            click_on 'Entrar como proprietário'
             click_on 'Início'
             
             expect(current_path).to eq root_path
@@ -44,16 +44,68 @@ describe 'Visitor log in' do
     end
 
     context 'for the first time (sign up)' do
-        it 'successfully' do
+        it 'successfully as property owner' do
             visit root_path
-            click_on 'Entrar'
-            click_on 'Sign up'
+            click_on 'Entrar como proprietário'
+            click_on 'Cadastrar'
             fill_in 'Email', with: 'owner@domain.com'
             fill_in 'Senha', with: '123456'
             fill_in 'Confirmação de Senha', with: '123456'
-            click_on 'Sign up'
+            click_on 'Cadastrar'
 
             expect(page).to have_content('Cadastro efetuado com sucesso')
+        end
+
+        it 'successfully as user' do
+            visit root_path
+            click_on 'Entrar como usuário'
+            click_on 'Cadastrar'
+            fill_in 'Email', with: 'user@domain.com'
+            fill_in 'Senha', with: '123456'
+            fill_in 'Confirmação de Senha', with: '123456'
+            click_on 'Cadastrar'
+
+            expect(page).to have_content('Cadastro efetuado com sucesso')
+        end
+    end
+
+    context 'as user' do
+        it 'successfully' do
+            user = User.create!(email: 'usuario@mail.com.br', password: '123456')
+
+            visit root_path
+            click_on 'Entrar como usuário'
+            fill_in 'Email', with: user.email
+            fill_in 'Senha', with: user.password
+            within 'form' do
+                click_on 'Entrar'
+            end
+
+            expect(page).to have_content('Login efetuado com sucesso!')
+            expect(page).to have_content(user.email)
+            expect(page).to have_link('Logout')
+            expect(page).not_to have_content('Entrar como usuário')
+        end 
+
+        it 'and logs out' do
+            user = User.create!(email: 'usuario@mail.com.br', password: '123456')
+
+            login_as user, scope: :user
+            visit root_path
+            click_on 'Logout'
+
+            expect(page).to have_content('Saiu com sucesso')
+            expect(page).not_to have_link('Logout')
+            expect(page).to have_link('Entrar como proprietário')
+            expect(page).not_to have_content(user.email)
+        end
+
+        it 'but decide to return to home' do
+            visit root_path
+            click_on 'Entrar como usuário'
+            click_on 'Início'
+            
+            expect(current_path).to eq root_path
         end
     end
 end
